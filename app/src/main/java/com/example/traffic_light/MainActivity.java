2226d13e870af625b2f1e5464653ee69f6c55ae6
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-03-04 16:57:26 +0300

    init commit2

--- app/src/main/java/com/example/traffic_light/MainActivity.java
+++ app/src/main/java/com/example/traffic_light/MainActivity.java
@@ -2,71 +2,28 @@ package com.example.traffic_light;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button red;
-    private Button yellow;
-    private Button green;
-    private View view;
-    private int currentBackgroundColor;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Определение элементов экрана по id
-        red = findViewById(R.id.red_button);
-        yellow = findViewById(R.id.yellow_button);
-        green = findViewById(R.id.green_button);
-        view = findViewById(R.id.activity);
-
-        //установка обработчика для кнопок
-        red.setOnClickListener(this);
-        yellow.setOnClickListener(this);
-        green.setOnClickListener(this);
     }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        currentBackgroundColor = Color.TRANSPARENT;
-        Drawable background = view.getBackground();
-        if (background instanceof ColorDrawable)
-            currentBackgroundColor = ((ColorDrawable) background).getColor();
-
-        outState.putInt("background_color", currentBackgroundColor);
+    public void onButtonRed(View view){
+        RelativeLayout back = findViewById(R.id.my_layout_id);
+        back.setBackgroundColor(getColor(R.color.red));
     }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-        if (savedInstanceState != null && savedInstanceState.containsKey("background_color")) {
-            view.setBackgroundColor(savedInstanceState.getInt("background_color"));
-        }
+    public void onButtonYellow(View view){
+        RelativeLayout back = findViewById(R.id.my_layout_id);
+        back.setBackgroundColor(getColor(R.color.yellow));
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.red_button:
-                view.setBackgroundColor(Color.RED);
-                break;
-            case R.id.yellow_button:
-                view.setBackgroundColor(Color.YELLOW);
-                break;
-            case R.id.green_button:
-                view.setBackgroundColor(Color.GREEN);
-                break;
-        }
+    public void onButtonGreen(View view){
+        RelativeLayout back = findViewById(R.id.my_layout_id);
+        back.setBackgroundColor(getColor(R.color.green));
     }
 }
